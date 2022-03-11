60055f2d4d20d5e1223d07f10c82b03b54f4d731
Murat Can Dogan <87142420+doganmuratcan@users.noreply.github.com>
2022-03-11 15:28:30 +0300

    Add files via upload

---
+++ Weather_Observation_Station_10.sql
@@ -0,0 +1,3 @@
+--Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION WHERE UPPER(SUBSTR(CITY, -1, 1)) NOT IN ('A', 'E', 'I', 'O', 'U');
